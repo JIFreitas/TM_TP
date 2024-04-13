9669eaafe2a7be0b0135932200d2c69149041e9f
Gabriel Brandão <gabrielbrandao@ipvc.pt>
2024-04-13 18:37:49 +0100

    starting loading screen

---
+++ Assets/scripts/menuPrincipalManager.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuPrincipalManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+
+    public void Jogar(){
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+    }
+
+    public void AbrirOpcoes(){
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+
+    public void FecharOpcoes(){
+        painelMenuInicial.SetActive(true);
+        painelOpcoes.SetActive(false);
+    }
+    public void SairJogo(){
+        Debug.Log("A sair do jogo...");
+        Application.Quit();
+    }
+}
